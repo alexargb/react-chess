a79ff3eb9da21dc00a9a1211163ab1a0dd23d14b
alexargb <alexarguellobueno@gmail.com>
2024-08-29 18:01:27 -0700

    fix: circle svg's opacity on ios

--- src/components/atoms/square/SquareMark.tsx
+++ src/components/atoms/square/SquareMark.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export const SquareMark = () => (
-  <svg role="square-mark" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="50" cy="50" r="12" opacity="0.7" />
+  <svg
+    role="square-mark"
+    viewBox="0 0 100 100"
+    visibility="visible"
+    xmlns="http://www.w3.org/2000/svg"
+    height="fit-content"
+  >
+    <circle cx="50" cy="50" r="12" />
   </svg>
 );
