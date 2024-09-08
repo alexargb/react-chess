f017d221e5e7d6944e0e3de7f9d6483522204e80
alexargb <alexarguellobueno@gmail.com>
2024-09-08 11:00:31 -0700

    fix: circle svg's height on ios

--- src/components/atoms/square/SquareMark.tsx
+++ src/components/atoms/square/SquareMark.tsx
@@ -6,6 +6,7 @@ export const SquareMark = () => (
     viewBox="0 0 100 100"
     visibility="visible"
     xmlns="http://www.w3.org/2000/svg"
+    height="100%"
   >
     <circle cx="50" cy="50" r="12" />
   </svg>
